0a261aa1cde1d5156e9507bb2185a620daa9d6e5
IamGatheru <davidgatheru490@gmail.com>
2024-10-16 02:15:10 +0300

    #Task

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+""" 0. Async Generator """
+
+from asyncio import sleep
+from random import uniform
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ Async Generator
+
+    Args:
+    None
+
+    Returns:
+        Generator: A list containing float | None
+    """
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
+        
